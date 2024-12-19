96f7034e150a756f288897fa07b46c7077102720
suphal-dev <suphal@klizos.com>
2024-12-19 15:36:56 +0530

    Button component added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
+import "./App.css";
+
+
+function App() {
+
+
+
+  return (
+    <>
+   <Button/> 
+     
+    </>
+  );
+}
+
+export default App;
